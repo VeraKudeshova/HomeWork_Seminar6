f545603804c0bbfa590abeb3d9d1d4d6b71c4184
Vera Kudeshova <BepaKy@yandex.ru>
2022-12-26 12:41:08 +0700

    Задача 43. Пересечения двух прямых.

--- Task2_S6/Program.cs
+++ Task2_S6/Program.cs
@@ -15,7 +15,7 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение k2");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = (-b2 + b1)/(-k1 + k2);
+double x = (b2 - b1)/(k1 - k2);
 double y = k2 * x + b2;
 
 Console.WriteLine($"Две прямые пересекутся в точке Z координатами X: {x}, Y: {y}");
